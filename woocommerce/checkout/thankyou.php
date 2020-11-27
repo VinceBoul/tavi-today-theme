b9b88b089d9e7020e5ccf8fbfbe5433b6475da02
jeremy_voisin <voisin.jeremy@outlook.fr>
2020-11-27 19:55:57 +0100

    Removed woocommerce configuration

--- woocommerce/checkout/thankyou.php
+++ woocommerce/checkout/thankyou.php
@@ -16,7 +16,7 @@
  */
 
 defined('ABSPATH') || exit;
-the_content();
+
 ?>
 
 <div class="woocommerce-order">
@@ -86,7 +86,7 @@ the_content();
 
     <?php else : ?>
 
-        <p class="woocommerce-notice woocommerce-notice--success woocommerce-thankyou-order-received"><?php echo apply_filters('woocommerce_thankyou_order_received_text', esc_html__('Thank you. Your order has been received.', 'woocommerce'), null); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+        the_content();
 
     <?php endif; ?>
 
